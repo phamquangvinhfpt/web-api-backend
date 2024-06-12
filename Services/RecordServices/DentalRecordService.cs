ccb440aa3d59fb9447c2230d3b5aeed0cfe8b322
ToanDC28 <toandcse160630@fpt.edu.vn>
2024-06-12 13:28:05 +0700

    Update DentalRecordService.cs

--- Services/RecordServices/DentalRecordService.cs
+++ Services/RecordServices/DentalRecordService.cs
@@ -6,6 +6,7 @@ using BusinessObject.Models;
 using DAO.Requests;
 using Repository.Appointments;
 using Repository.FollowUpAppointments;
+using Repository.MedicalRecords;
 using Repository.Prescriptions;
 using Repository.RecordRepositories;
 
@@ -17,17 +18,23 @@ namespace Services.RecordServices
         private readonly IAppointmentRepository appointmentRepository;
         private readonly IFollowUpAppointmentRepository followUpAppointmentRepository;
         private readonly IPrescriptionRepository prescriptionRepository;
+        private readonly IMedicalRecordRepository medicalRecordRepository;
         public DentalRecordService(){
             dentalRecordRepository = new DentalRecordRepository();
             appointmentRepository = new AppointmentRepository();
             followUpAppointmentRepository = new FollowUpAppointmentRepository();
             prescriptionRepository = new PrescriptionRepository();
+            medicalRecordRepository = new MedicalRecordRepository();
         }
 
         public void CreateDentalRecord(CreateDentalRecordRequest request)
         {
             var appointment = appointmentRepository.CreateAppointment(request.appointment);
-            var dental = dentalRecordRepository.CreateDentalRecord(request);
+            var dental = dentalRecordRepository.CreateDentalRecord(appointment.Id);
+            medicalRecordRepository.CreateMedicalRecord(request.MedicalRecordRequest, appointment.Id, dental.Id);
+            followUpAppointmentRepository.CreateFollowAppointments(request.followUpAppointmentRequests, dental.Id);
+            prescriptionRepository.CreatePrescription(request.prescriptionRequests, dental.Id);
+
         }
 
         public List<DentalRecord> getAllRecord()
