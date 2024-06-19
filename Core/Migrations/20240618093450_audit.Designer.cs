690d0e1477355c74933179a5b72293ec8484f331
Phạm Quang Vinh <Vinhpqse160633@fpt.edu.vn>
2024-06-19 20:57:37 +0700

    Odata

--- Core/Migrations/20240618093450_audit.Designer.cs
+++ Core/Migrations/20240618093450_audit.Designer.cs
@@ -4,6 +4,7 @@ using BusinessObject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Core.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240618093450_audit")]
+    partial class audit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Repository/Clinics/IClinicsRepository.cs
+++ Repository/Clinics/IClinicsRepository.cs
@@ -12,11 +12,10 @@ namespace Repository.Clinics
         public List<Clinic> GetAllClinics();
         
         public Clinic GetClinicsById(Guid Id);
-        public void AddClinics(Clinic clinic);
+        public void AddClinics(Clinic clinic, Guid userId);
         
         public void UpdateClinics(Clinic clinic);
         
         public void DeleteClinics(Guid Id);
-
     }
 }
